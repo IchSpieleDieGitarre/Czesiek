97ce9b9dec2c62fde5a9722e1dbc8647a4f6f75b
IchSpieleDieGitarre <franekza10@gmail.com>
2025-05-05 23:18:48 +0200

    Delete docs/Czesiek2 directory

--- docs/Czesiek2/godzina.js
+++
@@ -1,8 +0,0 @@
-function pokazGodzine() {
-  const teraz = new Date();
-  const godzina = teraz.toLocaleTimeString();
-  document.getElementById("czas").textContent = godzina;
-}
-
-setInterval(pokazGodzine, 1000);
-pokazGodzine();
